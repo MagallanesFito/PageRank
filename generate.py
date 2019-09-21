3f130085d3df7a10be0ff96ac9805c531c297c3b
MagallanesFito <magallanesfito@gmail.com>
2019-09-21 14:37:13 -0700

    Imprved code generation

--- generate.py
+++ generate.py
@@ -6,15 +6,18 @@ Generates random database
 '''
 #Generate params for js init file
 def generateGlobalParameters(init_file,links,damping,epsilon):
+	init_file.write("/*THIS FILE WAS GENERATED AUTOMATICALLY using generate.py\n------------------------------\n*/\n")
 	init_file.write("const LINKS ="+str(links)+";\nconst DAMPING = "+str(damping)+";\nconst EPSILON="+str(epsilon)+";\n")
 file = open("words_alpha.txt",'r')
 js_file = open("js/init.js",'w')
 database = open("database.txt",'w')
 word_list = []
 words = []
+#Parameters are set here
 LINKS=5
 DAMPING = 0.85
 EPSILON = 0.0001
+#---------------
 generateGlobalParameters(js_file,LINKS,DAMPING,EPSILON)
 
 for line in file:
@@ -27,9 +30,10 @@ indices = [random.randint(0,len(words)-1) for i in range(LINKS)]
 js_file.write("function insertData(){\nvar trie = new Trie();\n")
 for index in indices:
 	#index = random.randint(0,len(words)-1)
-	js_file.write(word_list[index])
+	js_file.write("\t"+word_list[index])
 	database.write(words[index])
 js_file.write("return trie;\n}\n")
 file.close()
+js_file.write("/*THIS FILE WAS GENERATED AUTOMATICALLY using generate.py\n------------------------------*/\n")
 js_file.close()
 database.close()
--- js/init.js
+++ js/init.js
@@ -1,12 +1,17 @@
+/*THIS FILE WAS GENERATED AUTOMATICALLY using generate.py
+------------------------------
+*/
 const LINKS =5;
 const DAMPING = 0.85;
 const EPSILON=0.0001;
 function insertData(){
 var trie = new Trie();
-trie.insert('excogitated');
-trie.insert('superfinish');
-trie.insert('geophilidae');
-trie.insert('rettery');
-trie.insert('claudication');
+	trie.insert('expirer');
+	trie.insert('commemorating');
+	trie.insert('psychoses');
+	trie.insert('staggering');
+	trie.insert('redig');
 return trie;
 }
+/*THIS FILE WAS GENERATED AUTOMATICALLY using generate.py
+------------------------------*/
